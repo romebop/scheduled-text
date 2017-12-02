c18454a151ae602a5cf57eb36535c186084ac9b6
Richard Lee <richardjslee93@gmail.com>
2017-12-02 11:50:06 -0600

    Allow sending message via the Twitter API

--- server.js
+++ server.js
@@ -1,23 +1,27 @@
-const express = require('express');
-const app = express();
-const http = require('http').Server(app);
-const twilio = require('twilio');
+const express = require('express')
+const app = express()
+const http = require('http').Server(app)
+const twilio = require('twilio')
+const bodyParser = require('body-parser')
 
-app.set('port', (process.env.PORT || 3000));
-app.use(express.static(__dirname));
+const accountSid = 'AC1825baa6715668cc7ca186d32a1b57c7'
+const authToken = ''
+
+const client = new twilio(accountSid, authToken)
+
+app.use(bodyParser.urlencoded({ extended: true })); 
 
 app.get('/', function(req, res) {
-  res.sendFile(__dirname + '/index.html');
+  res.sendFile(__dirname + '/index.html')
 });
 
-const accountSid = 'AC1825baa6715668cc7ca186d32a1b57c7'; // insert Account SID here
-const authToken = '';   // insert auth token here
+app.post('/sendMessage', function(req, res) {
+  client.messages.create({
+		body: req.body.reminder_message,
+		to: '+17655865847',  // Text this number
+		from: '+14158181712 ' // From a valid Twilio number
+	})
+	.then((message) => console.log(message.sid))
+});
 
-const client = new twilio(accountSid, authToken);
-
-client.messages.create({
-    body: 'wooga chakka',
-    to: '+14159481805',  // Text this number
-    from: '+14158181712 ' // From a valid Twilio number
-})
-.then((message) => console.log(message.sid));
+app.listen(3000, () => console.log('Listening on port 3000!'))
